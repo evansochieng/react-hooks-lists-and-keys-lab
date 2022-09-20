b405c9aeb943715ba94a5e25243f62c6c0335b6f
Evans Ochieng <evansochieng121@gmail.com>
2022-09-20 22:25:38 +0300

    Completed lists and keys lab

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,13 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {/* render ProjectItem components here */}
+        {projects.map( (project) => {
+          const {id, name, about, technologies} = project;
+          return <ProjectItem key={id} name={name} about={about} technologies={technologies}/>
+        })}
+      </div>
     </div>
   );
 }
